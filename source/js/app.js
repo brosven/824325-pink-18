e36c7d9c598d46c638c8ce777aa620c23c946aa0
brosven <andreyzabrodin45@gmail.com>
2020-01-13 01:42:38 +0300

    page header padding-top

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,13 @@
 var pageHeader = document.querySelector('.page-header');
 var pageHeaderToggle = document.querySelector('.page-header__toggle');
+var pageRepresentation = document.querySelector('.page-representation');
 
 pageHeaderToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('page-header--close')) {
     pageHeader.classList.remove('page-header--close');
+    pageRepresentation.classList.remove('page-representation--close');
   } else {
     pageHeader.classList.add('page-header--close');
+    pageRepresentation.classList.add('page-representation--close');
   }
 });
