997137d1d6f126be428c331652c05d9f1780eaa4
brosven <andreyzabrodin45@gmail.com>
2020-01-15 19:56:04 +0300

    changed retina size

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,16 @@
 var pageHeader = document.querySelector('.page-header');
 var pageHeaderToggle = document.querySelector('.page-header__toggle');
 var pageRepresentation = document.querySelector('.page-representation');
+var downloadRepresentation = document.querySelector('.download-app__representation');
 
 pageHeaderToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('page-header--close')) {
     pageHeader.classList.remove('page-header--close');
     pageRepresentation.classList.remove('page-representation--close');
+    downloadRepresentation.classList.remove('download-app__representation--close');
   } else {
     pageHeader.classList.add('page-header--close');
     pageRepresentation.classList.add('page-representation--close');
+    downloadRepresentation.classList.add('download-app__representation--close');
   }
 });
