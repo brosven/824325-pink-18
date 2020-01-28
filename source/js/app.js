8a1fed9f3937545e6dbdfebf7f4a2d7c806839c1
brosven <andreyzabrodin45@gmail.com>
2020-01-28 21:40:53 +0300

    28 01 2020

--- source/js/app.js
+++ source/js/app.js
@@ -2,15 +2,18 @@ var pageHeader = document.querySelector('.page-header');
 var pageHeaderToggle = document.querySelector('.page-header__toggle');
 var pageRepresentation = document.querySelector('.page-representation');
 var downloadRepresentation = document.querySelector('.download-app__representation');
+var pageHeaderIconToggle = document.querySelector('.page-header__icon-toggle');
 
 pageHeaderToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('page-header--close')) {
     pageHeader.classList.remove('page-header--close');
     pageRepresentation.classList.remove('page-representation--close');
     downloadRepresentation.classList.remove('download-app__representation--close');
+    pageHeaderIconToggle.classList.remove('page-header__icon-toggle--hide');
   } else {
     pageHeader.classList.add('page-header--close');
     pageRepresentation.classList.add('page-representation--close');
     downloadRepresentation.classList.add('download-app__representation--close');
+    pageHeaderIconToggle.classList.add('page-header__icon-toggle--hide');
   }
 });
