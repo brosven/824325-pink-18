5d10b279d83f81dea01ddc7dcd594592cebbed1d
brosven <andreyzabrodin45@gmail.com>
2020-01-29 03:35:07 +0300

    inside sprite

--- source/js/app.js
+++ source/js/app.js
@@ -2,18 +2,20 @@ var pageHeader = document.querySelector('.page-header');
 var pageHeaderToggle = document.querySelector('.page-header__toggle');
 var pageRepresentation = document.querySelector('.page-representation');
 var downloadRepresentation = document.querySelector('.download-app__representation');
-var pageHeaderIconToggle = document.querySelector('.page-header__icon-toggle');
+var pageHeaderIconMenu = document.querySelector('.page-header___icon-menu');
 
 pageHeaderToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('page-header--close')) {
     pageHeader.classList.remove('page-header--close');
     pageRepresentation.classList.remove('page-representation--close');
     downloadRepresentation.classList.remove('download-app__representation--close');
-    pageHeaderIconToggle.classList.remove('page-header__icon-toggle--hide');
+    pageHeaderIconMenu.classList.remove('page-header__icon-toggle--hide');
+    pageHeaderToggle.classList.remove('page-header__toggle--burger-width');
   } else {
     pageHeader.classList.add('page-header--close');
     pageRepresentation.classList.add('page-representation--close');
     downloadRepresentation.classList.add('download-app__representation--close');
-    pageHeaderIconToggle.classList.add('page-header__icon-toggle--hide');
+    pageHeaderIconMenu.classList.add('page-header__icon-toggle--hide');
+    pageHeaderToggle.classList.add('page-header__toggle--burger-width');
   }
 });
